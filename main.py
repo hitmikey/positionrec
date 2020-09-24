1a1755e85957fa902bd729d11dd6d9f500a48211
Mike <michaelgabriele@gmail.com>
2020-09-23 22:43:22 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 
 """ filename and sheet locations"""
 filename_time = "." + time.strftime("%Y%m%d-%H%M%S")
-filenames = {'int': './internal.xlsx', 'pb': './pb.xlsx', 'admin': './admin_falsebreak.xlsx'}
+filenames = {'int': './internal.xlsx', 'pb': './pb.xlsx', 'admin': './admin.xlsx'}
 sheets = {'int': 'Internal', 'pb': 'PB', 'admin': 'Admin'}
 
 """ setup internal dataframe """
@@ -16,6 +16,7 @@ df_int = pd.read_excel(
     usecols=[0, 2, 4, 5],
     names=['Type', 'ISIN', 'Quantity', 'Price']
 )
+df_int = df_int[df_int['Type'] == 'Bond']
 df_int = df_int.sort_values(by=['Type', 'ISIN']).reset_index()
 df_int['Price'] = df_int['Price'] * 100
 df_int_agg = df_int.groupby(['Type', 'ISIN']).agg({
@@ -45,6 +46,7 @@ df_admin = pd.read_excel(
     usecols=[3, 5, 6, 7],
     names=['ISIN', 'Name', 'Price', 'Quantity']
 )
+
 conditions_admin = [
     ((df_admin['Name'].str.startswith('STOCK LOAN FEE', na=False)) | (df_admin['Name'].str.startswith('TICKET CHARGES', na=False))),
     ((df_admin['Name'].str.startswith('EURO', na=False)) | (df_admin['Name'].str.startswith('U S DOLLARS', na=False))),
@@ -56,6 +58,7 @@ conditions_admin = [
 ]
 values_admin = ['Expenses', 'Cash', 'FX Forward', 'Repurchase Agreement', 'CDS', 'Bond', 'Other']
 df_admin['Type'] = np.select(conditions_admin, values_admin)
+df_admin = df_admin[df_admin['Type'] == 'Bond']
 df_admin = df_admin.sort_values(by=['Type', 'ISIN']).reset_index()
 df_admin_agg = df_admin.groupby(['Type', 'ISIN']).agg({
     'Quantity': 'sum',
@@ -64,18 +67,28 @@ df_admin_agg = df_admin.groupby(['Type', 'ISIN']).agg({
 
 """ setup comparison tool """
 diff_cols = ['Quantity', 'Price']
+
 df_agg_diffs_int_pb = df_int_agg[diff_cols] - df_pb_agg[diff_cols]
 df_agg_diffs_int_pb = df_agg_diffs_int_pb.loc[df_agg_diffs_int_pb['Quantity'].fillna(0) != 0]
+
 df_agg_diffs_int_admin = df_int_agg[diff_cols] - df_admin_agg[diff_cols]
 df_agg_diffs_int_admin = df_agg_diffs_int_admin.loc[df_agg_diffs_int_admin['Quantity'].fillna(0) != 0]
 
-""" run comparison and write csv """
 df_agg_results = pd.concat({'Int vs. PB': df_agg_diffs_int_pb, 'Int vs. Admin': df_agg_diffs_int_admin}, names=['Source'])
-df_agg_results.to_csv(r'csv_results' + filename_time + '.csv')
 
-""" print results """
+""" optional print individual results """
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
+print('\n', 'Internal vs. PB - BREAKS', '\n', df_agg_diffs_int_pb)
+print('\n', 'Internal vs. Admin - BREAKS', '\n', df_agg_diffs_int_admin)
 print('\n', '\n', '  Master Break List  ', '\n', df_agg_results)
+
+# print(df_admin['ISIN'='US105756BY51'])
+# print(df_int['ISIN'='US105756BY51'])
+
+""" optional write to csv """
+# df_agg_results.to_csv(r'csv_results' + filename_time + '.csv')
+# df_admin.to_csv(r'csv_admin_map' + filename_time + '.csv')
+# df_int.to_csv(r'csv_internal_map' + filename_time + '.csv')
